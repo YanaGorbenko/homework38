f8816788178c85cc47ba287ac46c95e48b50d38e
Yana Horbenko <yanagorbenko33@gmail.com>
2026-03-02 20:01:17 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { students } from './data.ts';
 
+let keyCount = 0;
+
 const markup = (
   <ul>
     {students.map(student => {
       return (
-        <li>
+        <li key={keyCount}>
           <p>Ім'я студента: {student.name}</p>
           <p>Вік студента: {student.age}</p>
           <p>Курс навчання: {student.course}</p>
-          <img src={student.avatar}></img>
+          <img src={student.avatar} alt="avatar" />
           {student.isOnline ? <p>Online</p> : <p>Offline</p>}
         </li>
       );
